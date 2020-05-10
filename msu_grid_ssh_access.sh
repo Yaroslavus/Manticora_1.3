d07496b20ed426a252ead8ef1e2abe7bec5808ba
Yaroslavus <yaroslavsagan@jinr.ru>
2020-05-10 06:21:42 +0300

    MANUAL.txt fully rewrited. file msu_grid... was changed.

--- msu_grid_ssh_access.sh
+++ msu_grid_ssh_access.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sshpass -p 'Neex8Tae' ssh -t sagan@gridmsu31.sinp.msu.ru 'cd /k1/taiga2/DATA_IACT; mc'
+
+#~$ scp -r sagan@gridmsu31.sinp.msu.ru:/k1/taiga2/DATA_IACT/2019-20/feb20/210220.02/ /home/yaroslav/Yaroslavus_GitHub/
